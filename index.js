8c8b767738c21f7e751b152bd5d3b95b8663441f
mohit <mohit.tiwari_cs18@gla.ac.in>
2021-06-07 00:41:56 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = process.env.port || 4500 
+const port = Process.env.port || 4500 
 
 
 app.listen(port, () => console.log(`Api is live`))
